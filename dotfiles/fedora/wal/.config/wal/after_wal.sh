24d54a0990bfc6801ab2743bb63f5d6f8ad3c51d
Nathan <nathan.kift@gmail.com>
2023-02-19 10:40:27 -0800

    Remove dmenu. Add rofi and pywal

---
+++ dotfiles/fedora/wal/.config/wal/after_wal.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Source the colors from wal
+source "${HOME}/.cache/wal/colors.sh"
+
+ln -sf "${HOME}/.cache/wal/dunstrc" "${HOME}/.config/dunst/dunstrc"
+
+# Restart dunst with the new color scheme
+pkill dunst
+dunst &
