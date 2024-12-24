6c94301c26f2aa3bdd2cd4282058c182bee926de
Nathan <nathan.kift@gmail.com>
2024-12-23 21:52:22 -0800

    Small fix for alacritty config format

--- dotfiles/fedora/polybar/.config/polybar/scripts/dnf-updates.sh
+++ dotfiles/fedora/polybar/.config/polybar/scripts/dnf-updates.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-updates=$(dnf updateinfo -q --list | wc -l)
+updates=$(dnf updateinfo -q list | wc -l)
 
 if [ "$updates" -gt 0 ]; then
     echo "󰏔 $updates"
 else
-    echo ""
+    echo "󰏔 0"
 fi
