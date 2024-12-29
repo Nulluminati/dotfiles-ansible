eca4fad28ed67d014fbb009b7bc1d9d591dceec6
Nathan <nathan.kift@gmail.com>
2024-12-28 16:43:57 -0800

    feat: 🎨 enhance llmgitmsg function with better commit message display

--- dotfiles/fedora/fish/.config/fish/functions/ai.fish
+++ dotfiles/fedora/fish/.config/fish/functions/ai.fish
@@ -3,27 +3,36 @@ function llmchat --description "Chat with AI (claude-3.5-sonnet-latest)"
 end
 
 function llmgitmsg --description "Create a git commit message using AI"
-	git add -A .
-
+	function read_input
+        set -l prompt $argv[1]
+        read -P $prompt reply
+        echo $reply
+    end
+	
 	function generate_commit_message
 		git diff --minimal --cached | llm -t gitcommit
 	end
 
+    git add -A .
     set commit_message (generate_commit_message $argv --retry "")
 
     while true
-        echo -e "Proposed commit message:\n"
-        echo $commit_message
-        echo -e ""
-
-        set choice (read_input "Do you want to (a)ccept or (r)egenerate? ")
-
-		set short_message (string split -m 1 "\n" $commit_message)[1]
+    	set short_message (string split -m 1 "\n" $commit_message)[1]
 		set description (string split -m 1 "\n" $commit_message)[2]
 
 		if test -z "$description"
 			set description ""
 		end
+
+        echo -e "Proposed commit message:\n"
+        echo $short_message
+        echo -e ""
+
+        echo -e "Proposed commit description:\n"
+        echo $description
+        echo -e ""
+
+        set choice (read_input "Do you want to (a)ccept or (r)egenerate? ")
 
         switch $choice
             case 'a' 'A' ''
