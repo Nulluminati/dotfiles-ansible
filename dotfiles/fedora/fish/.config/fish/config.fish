37ad1cec170c552773def9172f55de7554570705
Your Name <nathan.kift@gmail.com>
2025-01-04 23:18:22 -0800

    feat: add XDG env vars and extract utility
    Add XDG base directory spec environment variables, set delta as default pager, and add extract function for handling compressed files. Remove IP alias in favor of using actual IP tools.

--- dotfiles/fedora/fish/.config/fish/config.fish
+++ dotfiles/fedora/fish/.config/fish/config.fish
@@ -1,7 +1,19 @@
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_DESKTOP_DIR $HOME/Desktop
+set -gx XDG_DOWNLOAD_DIR $HOME/Downloads
+set -gx XDG_DOCUMENTS_DIR $HOME/Documents
+set -gx XDG_MUSIC_DIR $HOME/Music
+set -gx XDG_PICTURES_DIR $HOME/Pictures
+set -gx XDG_VIDEOS_DIR $HOME/Videos
+
 set -gx KUBE_EDITOR nvim
 set -gx VISUAL nvim
 set -gx EDITOR nvim
 set -gx BROWSER firefox
+set -gx PAGER delta
+
 
 set -U fish_greeting ""
 
--- dotfiles/fedora/fish/.config/fish/functions/_alias.fish
+++ dotfiles/fedora/fish/.config/fish/functions/_alias.fish
@@ -1,7 +1,4 @@
 function grep; command grep --color=auto $argv; end
-
-function ip; command dig +short myip.opendns.com @resolver1.opendns.com; end
-
 function l; command eza -laghF --git --icons --group-directories-first --sort name $argv; end
 function ll; command eza -laghF --git --icons --group-directories-first --sort modified $argv; end
 function la; command eza -laF --icons; end
