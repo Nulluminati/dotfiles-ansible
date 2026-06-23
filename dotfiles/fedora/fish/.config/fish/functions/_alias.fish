e5b3cdd585a196d92f1cc2c7d57821c306bcf6f4
Your Name <nathan.kift@gmail.com>
2026-06-23 10:44:14 -0700

    fix(fish): pass argv through la alias

--- dotfiles/fedora/fish/.config/fish/functions/_alias.fish
+++ dotfiles/fedora/fish/.config/fish/functions/_alias.fish
@@ -4,5 +4,5 @@ function gsubla --description "Open all changed files (staged, unstaged, untrack
 function gsubld --description "Open files changed from HEAD (staged + unstaged) in Sublime Text"; git diff --name-only HEAD | xargs -r subl $argv; end
 function l --description "List files with details, sorted by name"; command eza -laghF --git --icons --group-directories-first --sort name $argv; end
 function ll --description "List files with details, sorted by modification time"; command eza -laghF --git --icons --group-directories-first --sort modified $argv; end
-function la --description "List all files including hidden"; command eza -laF --icons; end
+function la --description "List all files including hidden"; command eza -laF --icons $argv; end
 function tree --description "Show directory tree with icons"; command eza --tree --icons $argv; end
